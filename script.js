c0f4e7d8ecf62e943dd23fda99e1af9631ec5cb1
taiarima <tai.arima@colorado.edu>
2023-04-26 14:08:33 +0300

    Setting up basic structure.

---
+++ script.js
@@ -0,0 +1,14 @@
+`use strict`;
+
+// Selecting Elements
+const logo = document.querySelector(".logo");
+const title = document.querySelector(".title");
+const chooseListLink = document.querySelector(".choose-list-link");
+const settingsLink = document.querySelector(".settings-link");
+const aboutLink = document.querySelector(".about-link");
+const helpLink = document.querySelector(".help-link");
+const appContainer = document.querySelector(".app-container");
+const hanziPrompt = document.querySelector(".hanzi-prompt");
+const answerText = document.querySelector(".answer-text");
+const checkAnsBtn = document.querySelector(".check-ans-btn");
+const undoBtn = document.querySelector(".undo-btn");

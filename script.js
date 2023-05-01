9a86910058952a638f9dc30eef3451ebe35982a0
taiarima <tai.arima@colorado.edu>
2023-05-01 13:55:09 +0300

    Started skeleton of first modal.

--- script.js
+++ script.js
@@ -51,31 +51,26 @@ nextBtn.addEventListener("click", function () {
   // empty function for now
 });
 
-
-
 // Coding Modal window
 // Get the modal
-const modal = document.getElementById("myModal");
-
-// Get the link that opens the modal
-const link = document.querySelector(".choose-list-link");
+const modalWindow = document.getElementById("modal-window");
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+const closeModalBtn = document.querySelector(".close-modal-btn");
 
 // When the user clicks the link, open the modal
-link.onclick = function () {
-  modal.style.display = "block";
+chooseListLink.onclick = function () {
+  modalWindow.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
+closeModalBtn.onclick = function () {
+  modalWindow.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalWindow) {
+    modalWindow.style.display = "none";
   }
 };
